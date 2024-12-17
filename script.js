71802c0f42e55e03ccb691c898afd74e7546f4ce
neerajyuvasoft454 <neerajpawar720@gmail.com>
2024-12-17 11:08:42 +0530

    change file after clone

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ setInterval(()=>{
     }
     myFun()
 },2000)
-console.log("change script file")
+console.log("change script file after cloaning")
 // Dark Fun
 dark.addEventListener("click",function(){
     document.querySelector("body").classList.toggle('active1')
